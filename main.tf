99968b341263ae5b9aa9bacd4820011ab3722746
egarelnabi <egarelnabi@users.noreply.github.com>
2020-03-11 12:42:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
 
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.12.12"
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   tags = {
-    Name = "${var.name}"
+    Name = var.name
   }
 }
